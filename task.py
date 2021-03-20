6a8ebce29dd16519b273c5b742b9cbca402cbf8c
Andrew L <64745401+ItIsAndrewL@users.noreply.github.com>
2021-03-20 15:30:53 -0400

    gonna merge better stuff

--- task.py
+++
@@ -1,8 +0,0 @@
-# Task object
-class task():
-    
-
-    def __init__(self, name: str, date, isComplete: bool):
-        self.name = name
-        self.date = date
-        self.isComplete = isComplete
