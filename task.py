00cc1f18d37ddfd356d0a14844291ed61b4f49dc
Booperboberino <septillionyt@gmail.com>
2021-03-20 15:13:06 -0400

    Task object base creation

--- task.py
+++ task.py
@@ -1,7 +1,8 @@
+# Task object
 class task():
     
 
-    def __init__(name: str, date: dateTime, isComplete: bool):
+    def __init__(name: str, date, isComplete: bool):
         self.name = name
         self.date = date
         self.isComplete = isComplete
