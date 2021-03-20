21aec2a5a57fcf4e5536456772867939a258365c
Booperboberino <septillionyt@gmail.com>
2021-03-20 15:51:19 -0400

    Created a loader.py with test data

---
+++ loader.py
@@ -0,0 +1,11 @@
+class loader():
+
+
+    # __init__(self):
+
+    # Loads every date 
+    def loadAllDates():
+        with open("txt/zpsw.txt", "r") as fp:
+            for i in fp:
+                print()
+        return 
