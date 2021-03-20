0f45fd10d4997b55f887f5d3a10dffff0a5d03c4
ItIsAndrewL <andrew.lockard15@gmail.com>
2021-03-20 15:19:56 -0400

    fix noahs first mistake

--- task.py
+++ task.py
@@ -2,7 +2,7 @@
 class task():
     
 
-    def __init__(name: str, date, isComplete: bool):
+    def __init__(self, name: str, date, isComplete: bool):
         self.name = name
         self.date = date
         self.isComplete = isComplete
