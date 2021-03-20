33f8e523326f6cf8478c1d6f7f6bcefcfa687c22
Booperboberino <septillionyt@gmail.com>
2021-03-20 17:15:42 -0400

    stuff

--- taskHandler.py
+++ taskHandler.py
@@ -1,16 +1,31 @@
+
+
 class TaskHandler():
 
     # def __init__():
     #     pass
+    
+    def findUniqueId():
 
-    def addTask():
+    # Does not return
+    def addTask(name: str, date):
+
         pass
 
-    def editTask():
+    # Does not return
+    def editTask(id:int, name: str, ):
+
         pass
 
+    # Does not return
     def removeTask():
-        pass
 
-    def numberOfTasksOnDay():
         pass
+    
+    # Get the number of tasks in one day
+    # Returns int
+    def numberOfTasksOnDay(day:dayType):
+        cnt = 0
+        for a in :   # Get list of tasks in one day
+            cnt += 1
+        return cnt
