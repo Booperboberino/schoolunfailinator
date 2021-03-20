8206d8c6e031d31368b15c9d0abcebca77211092
JoeMeister007 <joemeister007@gmail.com>
2021-03-20 16:07:50 -0400

    Commented

--- filetest.py
+++ filetest.py
@@ -1,15 +1,17 @@
-import os
-def ensureFoldersExist(fileLocation):
+import os #imports os
+def ensureFoldersExist(fileLocation): #function that I haven't tested cause I didn't have it in function form in IDLE, checks for folders of .txt and creates them if they don't exist to prevent error
     a = fileLocation.split('/')
-    for i in range(len(a)-1):
+    for i in range(len(a)-1): #goes through all the different folders, but not the file
         path = ""
-        for j in range(0,i+1):
+        for j in range(0,i+1): #assembles the path
             path+=a[j] +"/"
-        #print(path[len(path)-5:])
-        if not os.path.exists(path):
+
+        if not os.path.exists(path): #creates folder if it doesn't exist
             os.mkdir(path)
-string = '2021/4/20.txt'
+
+#just some code that I had to test it out
+string = '2021/4/20.txt' #file corresponding to the data of 4/20/2021
 ensureFoldersExist(string)        
-f = open(string,'a')
-f.write("testing")
+f = open(string,'a') #opens or creates the file
+f.write("testing") 
 f.close()
