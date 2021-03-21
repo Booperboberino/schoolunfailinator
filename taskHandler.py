485fc0e3bb7717c70dfaab550c1635d7dcc06c85
Booperboberino <septillionyt@gmail.com>
2021-03-20 21:32:04 -0400

    did

--- taskHandler.py
+++ taskHandler.py
@@ -1,31 +1,47 @@
+import loader
+from task import Task
 
+class TaskHandler:
 
-class TaskHandler():
-
-    # def __init__():
-    #     pass
+    def __init__(self):
+        self.tasks = []
+        pass
     
-    def findUniqueId():
+    def findUniqueId(self):
+        # (Get all ids)
+        allIds = []
+        cnt = 0
+        while True:
+            cnt += 1
+            if cnt in allIds:
+                pass
+            else:
+                return cnt
 
     # Does not return
-    def addTask(name: str, date):
+    def addTask(self, name: str, date):
+        id = findUniqueId()
+        task = Task(name, date)
+        self.tasks.append()
+
+        
+
+    def addWorkTast(self, id: int, date):
+
+    # Does not return
+    def editTask(self, id:int, name: str, ):
 
         pass
 
     # Does not return
-    def editTask(id:int, name: str, ):
-
-        pass
-
-    # Does not return
-    def removeTask():
+    def removeTask(self):
 
         pass
     
     # Get the number of tasks in one day
     # Returns int
-    def numberOfTasksOnDay(day:dayType):
+    def numberOfTasksOnDay(self, day:dayType):
         cnt = 0
-        for a in :   # Get list of tasks in one day
+        for a in loader.getListOfTasks:   # Get list of tasks in one day
             cnt += 1
         return cnt
